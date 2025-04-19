9fd5b83caba50de784f63ad6d0d71964deda1efe
Somanath patil <patilsomanath292@gmail.com>
2025-04-19 10:44:33 +0530

    this is ejs module

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 import express from 'express'
-
 const app = express()
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render("index");
 })
 
-app.get('/profile', (req, res) => {
-    res.send('Hello this is my profile')
-  })
+app.get('/contact', (req, res) => {
+  res.render("contact");
+})
 
-  app.get('/user', (req, res) => {
-    res.send('This is a user page')
-  })
-
-  app.get('/login', (req, res) => {
-    res.send('this is a login page')
-  })
-
-
-app.listen(3000)
+app.listen(3000);
